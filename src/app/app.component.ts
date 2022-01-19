ac5dcc6f488bbecb8c6bafb2235ee6542cf2e21b
JoanAndoni <mcr_joan@hotmail.com>
2022-01-19 18:55:50 +0000

    Adding the function on the component, the visualization of the search and adding mock data for the demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import houses from '../assets/mock-data/houses.json';
+
+interface House {
+  country: string;
+  address: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'search-app';
+
+  searchValue: string;
+  availableHouses: House[] = houses;
+  filteredHouses: House[] = [];
+  
+  constructor() {
+    this.searchValue = '';
+    this.filteredHouses = this.availableHouses;
+  }
+
+  makeSearch() {
+    this.filteredHouses = this.availableHouses.filter((house: House) => {
+      return (house.country.includes(this.searchValue) || house.address.includes(this.searchValue));
+    });
+  }
+
 }
